057dee9ef0ee6c79a19a04f1ba8a3fc97d73222f
Asif-102 <asif1zero2@gmail.com>
2022-10-29 01:35:36 +0600

    store all the instance and see all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Item:
     pay_rate = 0.8 # The pay rate after 20% discount
+    all = []
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
@@ -10,12 +11,29 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
 item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Laptop", 75, 5)
 
-print(Item.__dict__) # All the attributes for Class level
-print(item1.__dict__) # All the attributes for instance level
+print(Item.all)
+
+# print(Item.__dict__) # All the attributes for Class level
+# print(item1.__dict__) # All the attributes for instance level
+
+# item1.apply_discount()
+# print(item1.price)
+
+# item2.pay_rate = 0.7
+# item2.apply_discount()
+# print(item2.price)

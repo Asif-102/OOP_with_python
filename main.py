cd643609c72199def448d80ba29711d7d9fb79fd
Asif-102 <asif1zero2@gmail.com>
2022-10-29 01:08:15 +0600

    generate assertion error message

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Item:
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
-        assert price >= 0
-        assert quantity >= 0
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
 
         # Assign to self object
         self.name = name
@@ -13,7 +13,7 @@ class Item:
         return self.price * self.quantity
 
 
-item1 = Item("Phone", 100, 1)
+item1 = Item("Phone", -100, 1)
 item2 = Item("Laptop", 1000, 3)
 
 print(item1.calculate_total_price())
